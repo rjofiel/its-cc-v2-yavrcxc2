b5f9fc62bc2a0973e13c2bda6e03d09ef91c6507
Jofiel <62037537+rjofiel@users.noreply.github.com>
2026-04-08 14:06:30 +0200

    test: agregado test para actualizar el estado de un todo

--- src/services/todo.spec.ts
+++ src/services/todo.spec.ts
@@ -7,38 +7,49 @@ describe("TodoStore Service", () => {
     expect(store).toBeTruthy();
   });
 
-  it('should add todo with a title and priority', () => {
+  it("should add todo with a title and priority", () => {
     const store = new TodoStore();
-    store.addTodo('Task 1', 1);
+    store.addTodo("Task 1", 1);
 
     expect(store.todos()).toHaveLength(1);
-    expect(store.todos()[0].title).toEqual('Task 1')
-    expect(store.todos()[0].priority).toEqual(1)
-  })
+    expect(store.todos()[0].title).toEqual("Task 1");
+    expect(store.todos()[0].priority).toEqual(1);
+  });
 
-  it('should generate with an unique id', () => {
+  it("should generate with an unique id", () => {
     const store = new TodoStore();
-    store.addTodo('Task 1', 1);
-    store.addTodo('Task 1', 2);
+    store.addTodo("Task 1", 1);
+    store.addTodo("Task 1", 2);
 
-    expect(store.todos()[0].id).not.toEqual(store.todos()[1].id)
-    expect(store.todos()[0].title).toEqual(store.todos()[1].title)
-  })
+    expect(store.todos()[0].id).not.toEqual(store.todos()[1].id);
+    expect(store.todos()[0].title).toEqual(store.todos()[1].title);
+  });
 
-  it('should remove a todo by id', () => {
+  it("should remove a todo by id", () => {
     const store = new TodoStore();
-    store.addTodo('Task 1', 1);
+    store.addTodo("Task 1", 1);
 
     const todo = store.todos()[0];
 
     store.deleteTodo(todo.id);
     expect(store.todos()).toHaveLength(0);
-  })
+  });
 
-  it('should set completed as false by default ', () => {
+  it("should set completed as false by default ", () => {
     const store = new TodoStore();
-    store.addTodo('Task 1', 1);
+    store.addTodo("Task 1", 1);
 
     expect(store.todos()[0].completed).toBeFalsy();
-  })
+  });
+
+  it("should toggle todo completed status", () => {
+    const store = new TodoStore();
+
+    store.addTodo("Test", 1);
+    const todo = store.todos()[0];
+
+    store.toggleComplete(todo.id);
+
+    expect(store.todos()[0].completed).toBe(true);
+  });
 });
