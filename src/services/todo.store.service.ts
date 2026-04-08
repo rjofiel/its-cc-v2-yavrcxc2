bcb7220112cfe6cf668bfdb279ee2abbe62f807f
Jofiel <62037537+rjofiel@users.noreply.github.com>
2026-04-08 13:54:01 +0200

    refactor: creada interfaz para los objetos tipo Todo

--- src/services/todo.store.service.ts
+++ src/services/todo.store.service.ts
@@ -1,15 +1,16 @@
 import { Injectable, signal } from "@angular/core";
+import { Todo } from "../models/todo";
 
 @Injectable({
   providedIn: "root",
 })
 export class TodoStore {
-    private _todos = signal<Array<{title: string, priority: number, id: string}>>([]);
+    private _todos = signal<Array<Todo>>([]);
     
     public todos = this._todos.asReadonly();
 
     public addTodo = (title: string, priority: number) => {
-        const todo = {title, priority, id: crypto.randomUUID()}
+        const todo: Todo = {title, priority, id: crypto.randomUUID()}
         this._todos.update(todos => [...todos, todo]);
     }
 }
