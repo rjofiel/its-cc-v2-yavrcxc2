1acd54e577cdb51008bcd191d9a06a9d9c459847
Jofiel <62037537+rjofiel@users.noreply.github.com>
2026-04-08 14:50:32 +0200

    feat: agregado form para crear todos

--- src/todo-list/todo-list.ts
+++ src/todo-list/todo-list.ts
@@ -1,25 +1,34 @@
-import {  Component, signal, effect, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TodoStore } from '../services/todo.store.service';
+import { Component, signal, effect, inject } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { TodoStore } from "../services/todo.store.service";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { Todo } from "../models/todo";
 
 @Component({
-  selector: 'app-todo-list',
+  selector: "app-todo-list",
   imports: [CommonModule],
-  templateUrl: './todo-list.html',
+  templateUrl: "./todo-list.html",
 })
-export class TodoListComponent  {
+export class TodoListComponent {
+  private fb = inject(FormBuilder);
+
   public readonly store = inject(TodoStore);
-  public count = signal<number>(0)
+  
+  public count = signal<number>(0);
+
+  public todoForm: FormGroup = this.fb.group({
+    title: [""],
+    priority: [1],
+  });
 
   constructor() {
     effect(() => {
-      this.count.set(document.querySelectorAll('#todoList>li').length)
-    })
+      this.count.set(document.querySelectorAll("#todoList>li").length);
+    });
   }
 
   toggleClass(ev: MouseEvent, className: string) {
     const el = ev.currentTarget as HTMLElement;
     el?.classList.toggle(className);
   }
-
 }
