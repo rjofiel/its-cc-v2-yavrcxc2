20029e2ecd6ea2ed3bc4cab39d8b57cb5baa3120
Jofiel <62037537+rjofiel@users.noreply.github.com>
2026-04-08 13:52:55 +0200

    feat: agregado id en el objeto y uso de crypto para la generacion de ids

--- src/services/todo.store.service.ts
+++ src/services/todo.store.service.ts
@@ -4,11 +4,12 @@ import { Injectable, signal } from "@angular/core";
   providedIn: "root",
 })
 export class TodoStore {
-    private _todos = signal<Array<{title: string, priority: number}>>([]);
+    private _todos = signal<Array<{title: string, priority: number, id: string}>>([]);
     
     public todos = this._todos.asReadonly();
 
     public addTodo = (title: string, priority: number) => {
-        this._todos.update(todos => [...todos, {title, priority}]);
+        const todo = {title, priority, id: crypto.randomUUID()}
+        this._todos.update(todos => [...todos, todo]);
     }
 }
