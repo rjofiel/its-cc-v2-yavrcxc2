37a659b2bc4e9f52753bd585b05eca90b1e7fd1c
Jofiel <62037537+rjofiel@users.noreply.github.com>
2026-04-08 14:55:17 +0200

    feat: added reactive forms and create form section

--- src/todo-list/todo-list.ts
+++ src/todo-list/todo-list.ts
@@ -1,19 +1,19 @@
 import { Component, signal, effect, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { TodoStore } from "../services/todo.store.service";
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, ReactiveFormsModule } from "@angular/forms";
 import { Todo } from "../models/todo";
 
 @Component({
   selector: "app-todo-list",
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: "./todo-list.html",
 })
 export class TodoListComponent {
   private fb = inject(FormBuilder);
 
   public readonly store = inject(TodoStore);
-  
+
   public count = signal<number>(0);
 
   public todoForm: FormGroup = this.fb.group({
