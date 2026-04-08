c119ad1b3ccca4d7acaf3e7670d14f9ffaea225e
Jofiel <62037537+rjofiel@users.noreply.github.com>
2026-04-08 13:44:36 +0200

    feat: creado todo store service

--- src/services/todo.spec.ts
+++ src/services/todo.spec.ts
@@ -1,10 +1,9 @@
 /// <reference types="jest" />
+import { TodoStore } from "./todo.store.service";
 
-describe('TodoStore Service' , () => {
-
-    it('should TodoStore exists', () => {
-        const store = new TodoStore();
-        expect(store).toBeTruthy();
-
-    })
-})
+describe("TodoStore Service", () => {
+  it("should TodoStore exists", () => {
+    const store = new TodoStore();
+    expect(store).toBeTruthy();
+  });
+});
