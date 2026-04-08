b7621163753790d8e1f5041a72ce04b5808eb75a
Jofiel <62037537+rjofiel@users.noreply.github.com>
2026-04-08 13:57:19 +0200

    feat: agregado deleteTodo para eliminar todos

--- src/services/todo.store.service.ts
+++ src/services/todo.store.service.ts
@@ -5,12 +5,16 @@ import { Todo } from "../models/todo";
   providedIn: "root",
 })
 export class TodoStore {
-    private _todos = signal<Array<Todo>>([]);
-    
-    public todos = this._todos.asReadonly();
+  private _todos = signal<Array<Todo>>([]);
 
-    public addTodo = (title: string, priority: number) => {
-        const todo: Todo = {title, priority, id: crypto.randomUUID()}
-        this._todos.update(todos => [...todos, todo]);
-    }
+  public todos = this._todos.asReadonly();
+
+  public addTodo = (title: string, priority: number) => {
+    const todo: Todo = { title, priority, id: crypto.randomUUID() };
+    this._todos.update((todos) => [...todos, todo]);
+  };
+
+  public deleteTodo = (id: string) => {
+    this._todos.update((todos) => todos.filter((todo) => todo.id !== id));
+  };
 }
